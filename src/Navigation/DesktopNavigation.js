f5012249ad3ac75458584c65f15178970365fff2
Raul Garcia Breijo <raulgarcia10000@gmail.com>
2020-09-16 14:57:36 -0400

    Navigation menu for desktop devices

---
+++ src/Navigation/DesktopNavigation.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const DesktopNavigation = () =>
+{
+    return(
+        <div>   
+            <div> 
+                <p> Free College Stuff </p>
+            </div> 
+
+            <div> 
+                <li>
+                    <ul> Home </ul> 
+                    <ul> Contact </ul> 
+                </li> 
+            </div> 
+        </div>
+    )
+}
+
+export default DesktopNavigation; 
