746797038f37905545d16839e8361eee87dc34a8
Raul Garcia Breijo <raulgarcia10000@gmail.com>
2020-09-16 15:21:01 -0400

    mobile navigation component

---
+++ src/Navigation/mobileNavigation.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+const MobileNavigation = () =>
+{
+    return(
+        <div> 
+            <div> X </div>
+            <div> 
+                <li>
+                    <ul> Home </ul> 
+                    <ul> Contact </ul> 
+                </li> 
+            </div> 
+        </div> 
+    )
+}
+
+export default MobileNavigation; 
