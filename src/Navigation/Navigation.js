32f91a4a588ab0f0d67e8d03c0a8e583a42dff44
Raul Garcia Breijo <raulgarcia10000@gmail.com>
2020-09-17 20:23:40 -0400

    changed place of mobile nav

--- src/Navigation/Navigation.js
+++ src/Navigation/Navigation.js
@@ -1,13 +1,13 @@
 import React from 'react'; 
 import DesktopNavigation from './DesktopNavigation';
-
+import MobileNavigation from './mobileNavigation';
 
 const Navigation = () => 
 {
     // A condition that checks the screen size and displayes the one required either desktop or Mobile navigation?
     return(
         <div> 
-
+            <MobileNavigation/>
             <DesktopNavigation/>
         </div> 
     )
