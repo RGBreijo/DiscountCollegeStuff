1951fcbc46fef47bd021b4b2f038599cdac747ee
Raul Garcia Breijo <raulgarcia10000@gmail.com>
2020-09-18 16:05:23 -0400

    added class names for card style

--- src/DiscountCard/DiscountCard.js
+++ src/DiscountCard/DiscountCard.js
@@ -1,22 +1,26 @@
 import React from "react";
+import "./discountCard.css";
+
 
 const DiscountCard = (props) => 
 {
     return(
-        <div> 
-            <div> 
-                <img src ={props.itemImagePath} alt={props.itemAlt}/>
-            </div> 
-            <div> 
-                <h2> {props.itemName}</h2> 
+        <div className="cardContainer"> 
+        <div className="mainCardInformationContainer"> 
                 <div> 
-                    <p> Location: {props.location}</p>
-                    <p> Campus: {props.campus}</p>
-                    <p> Cost: {props.cost}</p>
+                    <img src ={props.itemImagePath} alt={props.itemAlt} width="100" height="100"/>
+                </div> 
+                <div className="cardTextContent"> 
+                    <h2 className="cardTitle"> {props.itemName}</h2> 
+                    <div className="cardInformation"> 
+                        <p className="infoContainer"> <span className="infoType"> Location: </span> <span className="infoValue"> {props.location} </span> </p>
+                        <p className="infoContainer"> <span className="infoType"> Campus:   </span> <span className="infoValue"> {props.campus}  </span> </p>
+                        <p className="infoContainer"> <span className="infoType"> Cost:     </span> <span className="infoValue"> {props.cost} </span> </p>
+                    </div> 
                 </div> 
             </div> 
 
-            <div> 
+            <div className="moreInformationContainer"> 
                 <a href={props.learnMore} target="_blank" rel="noopener noreferrer">Learn More</a>
             </div> 
         </div> 
