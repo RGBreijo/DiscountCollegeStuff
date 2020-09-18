819f4216fbc34965d4f9142e68ac03f98b12531a
Raul Garcia Breijo <raulgarcia10000@gmail.com>
2020-09-18 14:24:11 -0400

    added FIU colors

--- src/SchoolName/SchoolName.js
+++ src/SchoolName/SchoolName.js
@@ -1,11 +1,13 @@
 import React from "react"; 
+import './fiuSchool.css';
 
+// Need to see how to put in different style based on school name 
 
 const SchoolName = (props) => 
 {
     return(
         <div> 
-            <h1> {props.schoolName} </h1> 
+            <h1 className="fiuLogo"> {props.schoolName} </h1> 
         </div> 
     )
 }
