07163e3c46c0104262cce18a2143182dd14d8cbb
Raul Garcia Breijo <raulgarcia10000@gmail.com>
2020-09-18 14:51:13 -0400

    Open navigation when click on hamburger icon

--- src/icons/HamburgerIcon/HamburgerIcon.js
+++ src/icons/HamburgerIcon/HamburgerIcon.js
@@ -3,8 +3,14 @@ import './hamburgerIcon.css';
 
 const HamburgerIcon = () =>
 {
+    const showMobileNav = () =>
+    {
+        let mobileNavBarContainer = document.querySelector(".mobileNavContainer");
+        mobileNavBarContainer.style.width = "150px";
+    }
+
     return(
-        <div className="hamburger"> 
+        <div className="hamburger" onClick={showMobileNav}> 
             <div> </div> 
             <div> </div> 
             <div> </div> 
