17dc31154feb82d0fc43682683bf20b63188039a
Raul Garcia Breijo <raulgarcia10000@gmail.com>
2020-09-21 21:04:59 -0400

    added external style

--- src/SchoolName/SchoolName.js
+++ src/SchoolName/SchoolName.js
@@ -5,9 +5,13 @@ import './fiuSchool.css';
 
 const SchoolName = (props) => 
 {
+    let h1Classes = "fiuLogo " + props.test; 
+
     return(
         <div> 
-            <h1 className="fiuLogo"> {props.schoolName} </h1> 
+
+               <h1 className={h1Classes}> {props.schoolName} </h1> 
+         
         </div> 
     )
 }
