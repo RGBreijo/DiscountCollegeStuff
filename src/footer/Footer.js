a947b0b64112c3ae6c55e47eb286051a17f512e9
Raul Garcia Breijo <raulgarcia10000@gmail.com>
2020-10-10 15:17:11 -0400

    empty footer for layout purpose

---
+++ src/footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react"; 
+import "./Footer.css";
+
+
+const Footer = () => 
+{
+    return(
+        <div className="footerContainer"> 
+        </div>
+    )
+}
+
+export default Footer; 
