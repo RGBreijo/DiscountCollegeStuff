5dc87eece8951f8c989bde3ece487064a239ef29
Raul Garcia Breijo <raulgarcia10000@gmail.com>
2020-10-10 15:15:37 -0400

    removed contact page option

--- src/Navigation/DesktopNavigation/DesktopNavigation.js
+++ src/Navigation/DesktopNavigation/DesktopNavigation.js
@@ -13,7 +13,7 @@ const DesktopNavigation = () =>
 
                 <ul> 
                     <li> <a href="#"> Home</a> </li> 
-                    <li> <a href="#"> Contact</a> </li> 
+                    {/* <li> <a href="#"> Contact</a> </li>  */}
                 </ul>
 
             </div> 
