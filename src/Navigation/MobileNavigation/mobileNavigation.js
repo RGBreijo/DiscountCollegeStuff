9f8a2d891906f7cf9388a4cc732abacff2bf7d7b
Raul Garcia Breijo <raulgarcia10000@gmail.com>
2020-10-10 15:15:59 -0400

    removed contact page option

--- src/Navigation/MobileNavigation/mobileNavigation.js
+++ src/Navigation/MobileNavigation/mobileNavigation.js
@@ -23,7 +23,7 @@ const MobileNavigation = () =>
 
                 <ul>    
                     <li> <a href="#">Home</a>  </li>
-                    <li> <a href="#">Contact</a>  </li>
+                    {/* <li> <a href="#">Contact</a>  </li> */}
                 </ul> 
             </div> 
         </div> 
