da2785f4f6349c3710c05d8adb3b2f7291dc95d1
Raul Garcia Breijo <raulgarcia10000@gmail.com>
2020-10-13 21:02:01 -0400

    connected email signup

--- src/email/Email.js
+++ src/email/Email.js
@@ -1,8 +1,14 @@
+
+import MailchimpSubscribe from "react-mailchimp-subscribe";
 import React from "react"; 
 import "./email.css";
 
 const Email = () =>
 {
+
+    const URL = "https://gmail.us2.list-manage.com/subscribe/post?u=6f212258d1c2cdfd3a97ea0c0&amp;id=472fbd7c68";
+
+
     return(
         <div className="emailContainer">
             <div className="emailInfoContainer"> 
@@ -11,9 +17,18 @@ const Email = () =>
             </div>
 
             <div className="emailInputContainer"> 
-            <input type="email" id="email" name="email" placeholder="Email"/>
-            <button className="emailBtn" name="email" >Subscribe</button>
+            <MailchimpSubscribe url={URL}>
+                <div> 
+                <input type="email" id="mce-EMAIL" name="EMAIL" placeholder="Email"/>
+                {/* <div class="chimp" aria-hidden="true"><input type="text" name="b_6f212258d1c2cdfd3a97ea0c0_472fbd7c68" tabindex="-1" value=""/></div> */}
+                <button className="emailBtn" name="email" type="submit">Subscribe</button>
+                </div> 
+            </MailchimpSubscribe>
             </div>
+
+
+
+            
         </div> 
     )
 }
